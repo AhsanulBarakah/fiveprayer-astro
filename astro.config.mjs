6a6bd2eb91b3feb2ae78d32afb8b917399c1437f
jamaaldev <kabarojamaal@gmail.com>
2026-04-24 17:52:16 +0100

    Fix CSS on Vercel by using environment variable for base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  base: import.meta.env.PROD ? '/fiveprayer-astro/' : '/',
+  base: import.meta.env.PUBLIC_BASE || '/',
 });
